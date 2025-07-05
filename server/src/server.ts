e843dfc9cb3c63be909e726fa2fd4509a03bddd2
Hemanth Rasabhathula <hemanthrasabhathula@gmail.com>
2025-07-04 22:06:37 -0500

    feat: add keep-alive service and update dependencies in package.json

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,10 +3,15 @@ dotenv.config();
 
 import app from "./app";
 import { connectDB } from "./config/db";
+import cron from "node-cron";
+import { keepAliveService } from "./utils/helper";
 
 const PORT = process.env.PORT || 3000;
+
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
 });
+
+cron.schedule("*/5 * * * *", keepAliveService);
---
+++ server/src/utils/helper.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+const KEEP_ALIVE_URL =
+  process.env.BASE_URL ||
+  "https://best-lines-of-books-full-stack.onrender.com/";
+
+export const keepAliveService = async () => {
+  try {
+    const response = await axios.get(`${KEEP_ALIVE_URL}/health`);
+    if (response.status === 200) {
+      console.log("Keep-alive service is running successfully.");
+    } else {
+      console.error("Keep-alive service failed with status:", response.status);
+    }
+  } catch (error) {
+    console.error("Error occurred while checking keep-alive service:", error);
+  }
+};
